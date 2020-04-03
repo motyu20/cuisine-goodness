9fb227f53ad747c4ee7f1215a7a30356b8d579af
菊本　知花 <motyu20@kikumotochibananoMacBook-Air.local>
2020-04-03 17:31:47 +0900

    first

---
+++ script.js
@@ -0,0 +1,38 @@
+$(function(){
+    $('#login-show').click(function() {
+        $('#login-modal').fadeIn();
+    });
+    $('.signup-show').click(function() {
+        $('#signup-modal').fadeIn();
+      });
+    $('.close-modal').click(function() {
+        $('#login-modal').fadeOut();
+        $('#signup-modal').fadeOut();
+    });
+    $('.lesson-hover').hover(
+        function() {
+           $(this).find('.text-contents').addClass('text-active');
+        },
+        function() {
+            $(this).find('.text-contents').removeClass('text-active');
+        }
+    );
+    $('.faq-list-item').click(function() {
+        var $answer = $(this).find('.answer');
+        if($answer.hasClass('open')) {
+            $answer.removeClass('open');
+            $answer.slideUp();
+            $(this).find('span').text('+');
+        } else {
+            $answer.addClass('open');
+            $answer.slideDown();
+            $(this).find('span').text('-');
+        }
+    });
+    var textValue = $('#text-form').val();
+    if(textValue == '') {
+        $('.error-message').text('理由を記入してください');
+      } else {
+        $('.error-message').text('');
+      }
+});
